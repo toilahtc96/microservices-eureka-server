e86ed4db76bf5c08ae23ce549fe93a92e5821508
toilahtc96 <toilahtc@gmail.com>
2021-04-19 22:08:51 +0700

    first commit

---
+++ src/main/java/com/server/serverregistry/ServerregistryApplication.java
@@ -0,0 +1,15 @@
+package com.server.serverregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/serverregistry/ServerregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.serverregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
